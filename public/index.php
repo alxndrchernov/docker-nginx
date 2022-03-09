7f2f6abeeeac91a5ca0b38d4efcf91cb5ffc15d9
achernov <a.chernov@comf5.ru>
2022-03-09 15:40:00 +0300

    add icon

--- public/index.php
+++ public/index.php
@@ -2,9 +2,12 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>hi</title>
+    <link rel="icon" href="icon.jpeg" type="image/x-icon">
+    <title>&#x1F49B;</title>
 </head>
 <body>
-hello
+<div style="height: 100%;width: 100%;position: absolute;justify-content: center;display: flex;align-items: center">
+    <h1>Hello!</h1>
+</div>
 </body>
 </html>
